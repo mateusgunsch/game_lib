9bfc274c3bda080045fb48db4ec18fd7c70c84c7
Mateus Gunsch <mateusgunsch01@gmail.com>
2024-11-18 14:36:29 -0400

    Form submit, submit handler and game list rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,61 @@
+import { useState } from "react"
+
 export default function App() {
+    const [games, setGames] = useState([])
+    const [title, setTitle] = useState("")
+    const [cover, setCover] = useState("")
+
+    const addGame = ({ title, cover }) => {
+        const id = Math.floor(Math.random() * 1000000)
+        const game = { id, title, cover }
+        setGames(state => [...state, game])
+    }
+
+    const handleSubmit = (ev) => {
+        ev.preventDefault()
+        addGame({ title, cover })
+        setTitle("")
+        setCover("")
+    }
+
     return(
         <div className="id">
             <h1>Biblioteca de Jogos</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="title">Título: </label>
-                    <input type="text" name="title" id="title" />
+                    <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="cover">Capa: </label>
-                    <input type="text" name="cover" id="cover" />
+                    <input
+                        type="text"
+                        name="cover"
+                        id="cover"
+                        value={cover}
+                        onChange={(e) => setCover(e.target.value)}
+                    />
                 </div>
                 <button type="submit">Adicionar à biblioteca</button>
             </form>
+
+            <div className="games">
+                {games.map((game) => (
+                    <div key={game.id}>
+                        <img src={game.cover} alt="Imagem de Capa" />
+                        <div>
+                            <h2>{game.title}</h2>
+                            <button>Remover</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
