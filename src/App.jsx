3a7022ef4d2677b89640f0b5473d13981db91c4d
Mateus Gunsch <mateusgunsch01@gmail.com>
2024-11-18 13:33:34 -0400

    Form structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 export default function App() {
-  return(
-    <h1>teste</h1>
-  )
+    return(
+        <div className="id">
+            <h1>Biblioteca de Jogos</h1>
+            <form>
+                <div>
+                    <label htmlFor="title">Título: </label>
+                    <input type="text" name="title" id="title" />
+                </div>
+                <div>
+                    <label htmlFor="cover">Capa: </label>
+                    <input type="text" name="cover" id="cover" />
+                </div>
+                <button type="submit">Adicionar à biblioteca</button>
+            </form>
+        </div>
+    )
 }
